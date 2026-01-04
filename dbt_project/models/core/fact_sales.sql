11a353b5f505bcdd86d99d3cd198fa6ac10ebb6c
Suprathika V <suprathika.v@gmail.com>
2026-01-03 22:52:44 -0600

    Add incremental loading strategy to fact_sales - 90% faster daily runs

--- dbt_project/models/core/fact_sales.sql
+++ dbt_project/models/core/fact_sales.sql
@@ -1,4 +1,11 @@
-﻿select
+﻿{{
+    config(
+        materialized='incremental',
+        unique_key='invoice_no'
+    )
+}}
+
+select
     invoice_no,
     customer_id,
     stock_code,
@@ -8,3 +15,8 @@
     quantity * unit_price as total_value,
     cast(invoice_date as date) as order_date
 from {{ ref('stg_online_retail') }}
+
+{% if is_incremental() %}
+    -- Only process records newer than the max date in the existing table
+    where cast(invoice_date as date) > (select max(order_date) from {{ this }})
+{% endif %}
